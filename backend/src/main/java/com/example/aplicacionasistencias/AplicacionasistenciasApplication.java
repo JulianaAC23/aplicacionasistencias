792ff6b3e8017635007d53fa4ea371d2dca76c2f
León Alarcón <juliaanalarconcar9923@gmail.com>
2025-05-12 11:40:11 -0500

    feat se crea estructura del proyecto backend y frontend

---
+++ backend/src/main/java/com/example/aplicacionasistencias/AplicacionasistenciasApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionasistencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionasistenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionasistenciasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/aplicacionasistencias/AplicacionasistenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionasistencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionasistenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
